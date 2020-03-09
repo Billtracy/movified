18ce79c6c688acf8d92eefa80790567ab090b65f
{bill <{billtracy001@gmail.com}>
2020-03-09 16:12:33 +0100

    new

--- app/Http/Controllers/InsertController.php
+++ app/Http/Controllers/InsertController.php
@@ -16,6 +16,7 @@ $short_description = $request->input('short_description');
 $image = $request->input('image');
 $trailer_url = $request->input('trailer_url');
 $showing_date = $request->input('showing_date');
+$voted = 0;
 $data=array('title'=>$title,"short_description"=>$short_description,"image"=>$image,"trailer_url"=>$trailer_url, "showing_date"=>$showing_date);
 DB::table('movies')->insert($data);
 echo "Record inserted successfully.<br/>";
