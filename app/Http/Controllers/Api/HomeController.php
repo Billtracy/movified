2ce7954f36359a3a269c6d18ebe59ed604f9039e
{bill <olajidesuduku@gmail.com>
2020-04-30 00:17:28 +0100

    new changess

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request; 
 use App\Http\Controllers\Controller; 
 use App\vote;
+use App\Votable;
+use App\Movie;
 use Illuminate\Support\Facades\Auth; 
 use Validator;
 
@@ -28,15 +30,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-            $movies = \App\Movie::all();
+            $movies = App\Movie::all();
             // $date = \App\ShowingDate::where('next_movie_night', 1)->firstOrFail();
             $data = ['movies' => $movies];
-            return response()->json(['response' => 'success', 'data' => $data,], $this->successStatus);
+            return response()->json(['data' => $data,], $this->successStatus);
     }
 
 
 
-public function vote (Request $request) {    
+public function vote (Request $request) {  
+    // validate the inputs  
     $validator = Validator::make($request->all(), 
                  [ 
                     'email' => 'required|between:3,64|email',
@@ -45,28 +48,25 @@ public function vote (Request $request) {
     if ($validator->fails()) {          
           return response()->json(['error'=>$validator->errors()], 401);
 } else{
+    // check if people can vote
+    $votable = App\Votable::find(1);
+    if ($votable->check_votes == 0){
+        return response()->json(['error'=> "you can't vote now"], 401);
+    }else{
     $vote = new vote();
     $vote->email = $request->input('email');
     $vote->movie_title = $request->input('movie_title');
+    // check if the person has voted before
     if (vote::where('email', '=', $vote->email)->exists()) {
         return response()->json(['error'=> 'you already voted'], 401);
      }else{
     $vote->save();
        // increment the voted field of the movie table
-    \App\Movie::find($vote->movie_title)->increment('voted');
+    App\Movie::find($vote->movie_title)->increment('voted');
     $success['voted'] =  ('you voted successfully');
     return response()->json(['success'=>$success], $this->successStatus); 
    }
 }
 }
-
-public function votable(){
-    $votable = \App\Votable::find(1);
-    if ($votable->check_votes == 1) {
-         return response()->json(['votable'=>'Can Vote']);
-    }
-    else{
-        return response()->json(['votable'=> 0]);
-    }
 }
 }
