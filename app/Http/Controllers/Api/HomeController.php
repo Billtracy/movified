79f4f96264d27a64628d6f418052c89fd2e9de77
{bill <olajidesuduku@gmail.com>
2020-04-30 01:14:32 +0100

    new changess

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-            $movies = \App\Movie::all();
+            $movies = Movie::all();
             $data = ['movies' => $movies];
             return response()->json(['data' => $data,], $this->successStatus);
     }
@@ -47,20 +47,21 @@ public function vote (Request $request) {
           return response()->json(['error'=>$validator->errors()], 401);
 } else{
     // check if people can vote
-    $votable = \App\Votable::find(1);
+    $votable = Votable::find(1);
     if ($votable->check_votes == 0){
         return response()->json(['error'=> "you can't vote now"], 401);
     }else{
+    $movie_id= Movie::where ('movie_title', '=', $request->input('movie_title'))->exists();
     $vote = new vote();
     $vote->email = Auth::user()->email;
-    $vote->movie_title = $request->input('movie_title');
+    $vote->movie_title = $movie_id;
     // check if the person has voted before
     if (vote::where('email', '=', $vote->email)->exists()) {
         return response()->json(['error'=> 'you already voted'], 401);
      }else{
     $vote->save();
        // increment the voted field of the movie table
-    \App\Movie::find($vote->movie_title)->increment('voted');
+    Movie::find($vote->movie_title)->increment('voted');
     $success['voted'] =  ('you voted successfully');
     return response()->json(['success'=>$success], $this->successStatus); 
    }
