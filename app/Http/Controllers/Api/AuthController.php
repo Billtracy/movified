a906560cf81fae827618cb508f0f448a16b0a70e
{bill <olajidesuduku@gmail.com>
2020-07-03 15:33:30 +0100

    mail

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,26 +7,26 @@ use Illuminate\Support\Facades\Auth;
 use Validator;
 class AuthController extends Controller 
 {
- public $successStatus = 200;
-  
- public function register(Request $request) {    
- $validator = Validator::make($request->all(), 
-              [ 
-              'name' => 'required',
-              'email' => 'required|between:3,64|email|unique:users',
-              'password' => 'required',  
-              'c_password' => 'required|same:password', 
-             ]);   
- if ($validator->fails()) {          
-       return response()->json(['error'=>$validator->errors()], 401);                        }    
- $input = $request->all();  
- $input['password'] = bcrypt($input['password']);
- $user = User::create($input);
-//  $user->sendApiEmailVerificationNotification();
+   public $successStatus = 200;
+   
+   public function register(Request $request) {    
+   $validator = Validator::make($request->all(), 
+               [ 
+               'name' => 'required',
+               'email' => 'required|between:3,64|email|unique:users',
+               'password' => 'required',
+               'c_password' => 'required|same:password', 
+               ]);   
+   if ($validator->fails()) {          
+         return response()->json(['error'=>$validator->errors()], 401);                        }    
+   $input = $request->all();  
+   $input['password'] = bcrypt($input['password']);
+   $user = User::create($input);
+   $user->sendApiEmailVerificationNotification();
 
-//  $success['message'] = 'Please confirm yourself by clicking on verify button sent to you on your email';
- $success['token'] =  $user->createToken('AppName')-> accessToken;
- return response()->json(['success'=>$success], $this-> successStatus);
+   $success['message'] = 'Please confirm yourself by clicking on verify button sent to you on your email';
+   $success['token'] =  $user->createToken('AppName')-> accessToken;
+   return response()->json(['success'=>$success], $this-> successStatus);
 }
   
    
