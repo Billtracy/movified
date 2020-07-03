5b39c43106fc1b189e88f32fda68a5d13fbcdcb1
{bill <olajidesuduku@gmail.com>
2020-07-03 16:46:25 +0100

    mail

--- app/User.php
+++ app/User.php
@@ -38,6 +38,10 @@ class User extends \TCG\Voyager\Models\User implements MustVerifyEmail
         'email_verified_at' => 'datetime',
     ];
 
+    public function sendApiEmailVerificationNotification()
+    {
+        $this->notify(new VerifyApiEmail); // my notification
+    }
     
     public function votes()
     {
