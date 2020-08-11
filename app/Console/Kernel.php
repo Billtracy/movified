19d742c81f129f7e2978b41eb5ce8ba41d995f68
Frank Wesley <billtracy0001@gmail.com>
2020-08-11 17:10:23 +0100

    movified

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('down')
+        $schedule->command('db:wipe --force')
                  ->everyMinute();
 
         // $schedule->command('db:wipe --force')->everyFiveMinutes()
