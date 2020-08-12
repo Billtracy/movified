885f0b59a9330ec770e641f18de17651ff7c9802
Frank Wesley <billtracy0001@gmail.com>
2020-08-12 12:18:52 +0100

    movify

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -61,7 +61,7 @@ class HomeController extends Controller
 
         $orders = $woocommerce->get('orders');
         foreach ($orders as $order){
-            return response()->json($order->billing->first_name);
+            return response()->json($order);
         }
     }
 
