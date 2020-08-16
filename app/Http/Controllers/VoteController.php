f29df796f7f1dcf80b08d8c68b13046a690f1ae2
Frank Wesley <billtracy0001@gmail.com>
2020-08-16 23:29:13 +0100

    bill

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -8,32 +8,17 @@ class VoteController extends Controller
 {
     public $successStatus = 200;
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         // Validate the request...
@@ -48,52 +33,27 @@ class VoteController extends Controller
 
         $vote->save();
 
-        // return response()->json(['success'=>$vote], $this->successStatus); 
+        // return response()->json(['success'=>$vote], $this->successStatus);
 
         return redirect('/')->with('status', "you successfully voted $request->movie_id;");
-        
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\vote  $vote
-     * @return \Illuminate\Http\Response
-     */
     public function show(vote $vote)
     {
         return view('vote');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\vote  $vote
-     * @return \Illuminate\Http\Response
-     */
     public function edit(vote $vote)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\vote  $vote
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, vote $vote)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\vote  $vote
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(vote $vote)
     {
         //
